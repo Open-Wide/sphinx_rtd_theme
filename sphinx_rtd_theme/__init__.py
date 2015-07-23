0eb60700f9320cadba9d980015720f7df471412e
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2015-07-23 10:38:56 +0200

    Moving role in directives.py file

--- sphinx_rtd_theme/__init__.py
+++ sphinx_rtd_theme/__init__.py
@@ -4,6 +4,7 @@ From https://github.com/ryan-roemer/sphinx-bootstrap-theme.
 
 """
 import os
+from sphinx_rtd_theme import directives
 
 VERSION = (0, 1, 8)
 
@@ -15,3 +16,6 @@ def get_html_theme_path():
     """Return list of HTML theme paths."""
     cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     return cur_dir
+
+def setup(app):
+    directives.setup(app)
--- sphinx_rtd_theme/directives.py
+++ sphinx_rtd_theme/directives.py
@@ -1,11 +1,3 @@
-#
-# tags.py
-#
-#    https://gist.github.com/tk0miya/8558c915187442a0f7dd
-#    
-#   Author: @tk0miya
-#   License: Apache 2.0
-#
 from docutils import nodes, utils
 import re
  
@@ -27,6 +19,15 @@ def tag_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
     return [raw], []
  
  
+def awesome_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    awesome_tag = '<i class="fa fa-%s"></i>' % utils.unescape(text)
+    raw = nodes.raw('', awesome_tag, format='html')
+    return [raw], []
+ 
+ 
 def setup(app):
     app.add_role('tag', tag_role)
     app.add_stylesheet('css/tag.css')
+    
+    app.add_role('awesome', awesome_role)
+    app.add_stylesheet('http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css')

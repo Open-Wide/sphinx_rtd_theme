08cf0f1511b61eb9e196ce8decd183b8d4e6b541
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2015-07-27 13:35:34 +0200

    Format tables

--- sphinx_rtd_theme/extensions/latex_mods.py
+++ sphinx_rtd_theme/extensions/latex_mods.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
-import os
-
-from docutils.io import FileOutput
-from docutils.frontend import OptionParser
 from docutils import nodes
-
+from docutils.frontend import OptionParser
+from docutils.io import FileOutput
+import os
 import sphinx.builders.latex
-from sphinx.util.smartypants import educate_quotes_latex
-from sphinx.writers.latex import LaTeXWriter
+from sphinx.locale import _
 from sphinx.util.console import bold
 from sphinx.util.osutil import copyfile
+from sphinx.util.smartypants import educate_quotes_latex
 from sphinx.util.texescape import tex_escape_map
 import sphinx.writers.latex
+from sphinx.writers.latex import LaTeXWriter
 
 # remove usepackage for sphinx here, we add it later in the preamble in conf.py
 sphinx.writers.latex.HEADER = sphinx.writers.latex.HEADER.replace('\usepackage{sphinx}', '\usepackage{openwide_sphinx}')
@@ -25,46 +24,155 @@ class LaTeXRTDTranslator(BaseTranslator):
         BaseTranslator.__init__(self, document, builder)
         if builder.config.language and builder.config.language != 'ja':
             self.elements['fncychap'] = '\\usepackage[Bjornstrup]{fncychap}'
+#        self.elements['usepackages'] += "\n" + '\usepackage{tcolorbox}'
+#        self.elements['usepackages'] += "\n" + '\usepackage{tabularx}'
+#        self.elements['usepackages'] += "\n" + '\usepackage{array}'
+#        self.elements['usepackages'] += "\n" + '\usepackage{colortbl}'
+#        self.elements['usepackages'] += "\n" + '\\tcbuselibrary{skins}'
+
+    def depart_table(self, node):
+        if self.table.rowcount > 30:
+            self.table.longtable = True
+        self.body = self._body
+        if not self.table.longtable:
+            self.body.append('\\begin{table}[h]\n')
+            self.body.append('\\centering\n')
+        if not self.table.longtable and self.table.caption is not None:
+            self.body.append(u'\n\n\\begin{threeparttable}\n'
+                             u'\\capstart\\caption{%s}\n' % self.table.caption)
+            for id in self.next_table_ids:
+                self.body.append(self.hypertarget(id, anchor=False))
+            if node['ids']:
+                self.body.append(self.hypertarget(node['ids'][0], anchor=False))
+            self.next_table_ids.clear()
+        if self.table.longtable:
+            self.body.append('\n\\begin{longtable}')
+            endmacro = '\\end{longtable}\n\n'
+        elif self.table.has_verbatim:
+            self.body.append('\n\\begin{tabular}')
+            endmacro = '\\end{tabular}\n\n'
+        elif self.table.has_problematic and not self.table.colspec:
+            # if the user has given us tabularcolumns, accept them and use
+            # tabulary nevertheless
+            self.body.append('\n\\begin{tabular}')
+            endmacro = '\\end{tabular}\n\n'
+        else:
+            self.body.append('\n\\begin{tabulary}{\\linewidth}')
+            endmacro = '\\end{tabulary}\n\n'
+        if self.table.colspec:
+            self.body.append(self.table.colspec)
+        else:
+            if self.table.has_problematic:
+                colwidth = 0.95 / self.table.colcount
+                colspec = ('p{%.3f\\linewidth}|' % colwidth) * \
+                    self.table.colcount
+                self.body.append('{|' + colspec + '}\n')
+            elif self.table.longtable:
+                self.body.append('{|' + ('l|' * self.table.colcount) + '}\n')
+            else:
+                self.body.append('{|' + ('L|' * self.table.colcount) + '}\n')
+        if self.table.longtable and self.table.caption is not None:
+            self.body.append(u'\\caption{%s}' % self.table.caption)
+            for id in self.next_table_ids:
+                self.body.append(self.hypertarget(id, anchor=False))
+            self.next_table_ids.clear()
+            self.body.append(u'\\\\\n')
+        if self.table.longtable:
+            self.body.append('\\hline\n')
+            self.body.extend(self.tableheaders)
+            self.body.append('\\endfirsthead\n\n')
+            self.body.append('\\multicolumn{%s}{c}%%\n' % self.table.colcount)
+            self.body.append(r'{{\textsf{\tablename\ \thetable{} -- %s}}} \\'
+                             % _('continued from previous page'))
+            self.body.append('\n\\hline\n')
+            self.body.extend(self.tableheaders)
+            self.body.append('\\endhead\n\n')
+            self.body.append(r'\hline \multicolumn{%s}{|r|}{{\textsf{%s}}} \\ \hline'
+                             % (self.table.colcount,
+                                _('Continued on next page')))
+            self.body.append('\n\\endfoot\n\n')
+            self.body.append('\\endlastfoot\n\n')
+        else:
+            self.body.append('\\hline\n')
+            self.body.extend(self.tableheaders)
+        self.body.extend(self.tablebody)
+        self.body.append(endmacro)
+        if not self.table.longtable and self.table.caption is not None:
+            self.body.append('\\end{threeparttable}\n\n')
+        if not self.table.longtable:
+            self.body.append('\\end{table}\n\n')
+        self.table = None
+        self.tablebody = None
+
+    def visit_row(self, node):
+        BaseTranslator.visit_row(self, node)
+        if isinstance(node.parent, nodes.thead):
+            self.body.append('\\headcol ')
+
+#    def depart_row(self, node):
+#        self.body.append('\\\\\n')
+#        if any(self.remember_multirow.values()):
+#            linestart = 1
+#            for col in range(1, self.table.col + 1):
+#                if self.remember_multirow.get(col):
+#                    if linestart != col:
+#                        linerange = str(linestart) + '-' + str(col - 1)
+#                        self.body.append('\\cline{' + linerange + '}')
+#                    linestart = col + 1
+#                    if self.remember_multirowcol.get(col, 0):
+#                        linestart += self.remember_multirowcol[col]
+#            if linestart <= col:
+#                linerange = str(linestart) + '-' + str(col)
+#                self.body.append('\\cline{' + linerange + '}')
+#        self.table.rowcount += 1
+
+    def visit_entry(self, node):
+        if self.table.col == 0:
+            while self.remember_multirow.get(self.table.col + 1, 0):
+                self.table.col += 1
+                self.remember_multirow[self.table.col] -= 1
+                if self.remember_multirowcol.get(self.table.col, 0):
+                    extracols = self.remember_multirowcol[self.table.col]
+                    self.body.append(' \multicolumn{')
+                    self.body.append(str(extracols + 1))
+                    self.body.append('}{|l|}{}')
+                    self.table.col += extracols
+                self.body.append(' & ')
+        else:
+            self.body.append(' & ')
+        self.table.col += 1
+        context = ''
+        if 'morecols' in node:
+            self.body.append(' \multicolumn{')
+            self.body.append(str(node.get('morecols') + 1))
+            if self.table.col == 1:
+                self.body.append('}{|l|}{')
+            else:
+                self.body.append('}{l|}{')
+            context += '}'
+        if 'morerows' in node:
+            self.body.append(' \multirow{')
+            self.body.append(str(node.get('morerows') + 1))
+            self.body.append('}{*}{')
+            context += '}'
+            self.remember_multirow[self.table.col] = node.get('morerows')
+        if 'morecols' in node:
+            if 'morerows' in node:
+                self.remember_multirowcol[self.table.col] = node.get('morecols')
+            self.table.col += node.get('morecols')
+        if isinstance(node.parent.parent, nodes.thead):
+            self.body.append('\\textsf{\\relax\\textcolor{white}{')
+            context += '}}'
+        while self.remember_multirow.get(self.table.col + 1, 0):
+            self.table.col += 1
+            self.remember_multirow[self.table.col] -= 1
+            context += ' & '
+            if self.remember_multirowcol.get(self.table.col, 0):
+                extracols = self.remember_multirowcol[self.table.col]
+                context += ' \multicolumn{'
+                context += str(extracols + 1)
+                context += '}{l|}{}'
+                self.table.col += extracols
+        self.context.append(context)
 
 sphinx.writers.latex.LaTeXTranslator = LaTeXRTDTranslator
-
-#class CustomLaTeXTranslator(LaTeXRTDTranslator):
-#    def astext(self):
-#            return (#HEADER % self.elements +
-#                    #self.highlighter.get_stylesheet() +
-#                    u''.join(self.body)
-#                    #'\n' + self.elements['footer'] + '\n' +
-#                    #self.generate_indices() +
-#                    #FOOTER % self.elements
-#                    )
-#
-#    def unknown_departure(self, node):
-#        if node.tagname == 'only':
-#            return
-#        return super(CustomLaTeXTranslator, self).unknown_departure(node)
-#
-#    def unknown_visit(self, node):
-#        if node.tagname == 'only':
-#            return
-#        return super(CustomLaTeXTranslator, self).unknown_visit(node)
-    
-#class CustomLaTeXBuilder(sphinx.builders.latex.LaTeXBuilder):
-#
-#    def write(self, *ignored):
-#        super(CustomLaTeXBuilder, self).write(*ignored)
-#
-#        backup_translator = sphinx.writers.latex.LaTeXTranslator
-#        sphinx.writers.latex.LaTeXTranslator = CustomLaTeXTranslator
-#        backup_doc = sphinx.writers.latex.BEGIN_DOC
-#        sphinx.writers.latex.BEGIN_DOC = ''
-#
-#        # output these as include files
-#        
-#        sphinx.writers.latex.LaTeXTranslator = backup_translator
-#        sphinx.writers.latex.BEGIN_DOC = backup_doc
-#
-#    def finish(self, *args, **kwargs):
-#        super(CustomLaTeXBuilder, self).finish(*args, **kwargs)
-#
-## monkey patch the shit out of it
-#sphinx.builders.latex.LaTeXBuilder = CustomLaTeXBuilder
